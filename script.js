51bfeb7bc7dca92504f04b6b10ccb34518807d5f
fredgyoung <fredgyoung@gmail.com>
2023-12-15 16:18:06 -0500

    more

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ window.addEventListener('load', function() {
             this.speedY = 0;
             this.dx = 0;
             this.dy = 0;
+            this.speedModifier = 20;
         }
         draw(context){
             context.beginPath();
@@ -35,10 +36,34 @@ window.addEventListener('load', function() {
         update(){
             this.dx = this.game.mouse.x - this.collisionX;
             this.dy = this.game.mouse.y - this.collisionY;
-            this.speedX = this.dx/20;
-            this.speedY = this.dy/20;
-            this.collisionX += this.speedX;
-            this.collisionY += this.speedY;
+            const distance = Math.hypot(this.dy, this.dx);
+            if (distance > this.speedModifier){
+                this.speedX = this.dx/distance || 0;
+                this.speedY = this.dy/distance || 0;    
+            } else {
+                this.speedX = 0;
+                this.speedY = 0;
+            }
+            this.collisionX += this.speedX * this.speedModifier;
+            this.collisionY += this.speedY * this.speedModifier;
+        }
+    }
+
+    class Obstacle {
+        constructor(game){
+            this.game = game;
+            this.collisionX = Math.random() * this.game.width;
+            this.collisionY = Math.random() * this.game.height;
+            this.collisionRadius = 100;
+        }
+        draw(context){
+            context.beginPath();
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+            context.save();
+            context.globalAlpha = 0.5;
+            context.fill();
+            context.restore();
+            context.stroke();
         }
     }
 
@@ -48,6 +73,8 @@ window.addEventListener('load', function() {
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
+            this.numberOfObstacles = 10;
+            this.obstacles = [];
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
@@ -75,10 +102,33 @@ window.addEventListener('load', function() {
         render(context){
             this.player.draw(context);
             this.player.update();
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
+        }
+        init(){
+            let attempts = 0;
+            while (this.obstacles.length < this.numberOfObstacles && attempts < 500){
+                let testObstacle = new Obstacle(this);
+                let overlap = false;
+                this.obstacles.forEach(obstacle => {
+                    const dx = testObstacle.collisionX - obstacle.collisionX;
+                    const dy = testObstacle.collisionY - obstacle.collisionY;
+                    const distance = Math.hypot(dy, dx);
+                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius;
+                    if (distance < sumOfRadii) {
+                        overlap = true;
+                    }
+                });
+                if (!overlap){
+                    this.obstacles.push(testObstacle);
+                }
+                attempts++;
+            }
         }
     }
 
     const game = new Game(canvas);
+    game.init();
+    console.log(game);
 
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
